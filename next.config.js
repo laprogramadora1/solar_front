9db056aa2aa22205670f5b0612997c62dc443304
laprogramadora1 <156002148+laprogramadora1@users.noreply.github.com>
2024-03-24 20:41:55 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,    
+    env:{
+        path: 'http://localhost:3000/',
+        path_media: 'http://localhost/energia/',
+        api: 'http://localhost:5000/'
+    }
+    
+
+}
 
 module.exports = nextConfig
